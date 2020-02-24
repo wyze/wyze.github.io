03778e8ec28730ff0461e4608f63507699d2d2ac
Neil Kistner <neil.kistner@gmail.com>
2020-02-24 12:39:08 -0600

    Fix more lighthouse things

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,9 @@ export default class extends Document {
             name="description"
             content="Neil Kistner is a self taught software engineer focused mainly on web applications. He loves open source and contributes to projects like Yarn and Tessel."
           />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         </Head>
         <body>
           <Main />
