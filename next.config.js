66630d1374dc0c1300266ad3efcd1ecc0e64a9b7
Neil Kistner <neil.kistner@gmail.com>
2020-02-24 10:13:52 -0600

    Properly configure service worker for Next/Now2.0

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+const NextWorkboxPlugin = require('next-workbox-webpack-plugin')
+
+const buildId = '1'
 
 module.exports = {
-  webpack: config => {
-    config.plugins.push(
-      new SWPrecacheWebpackPlugin({
-        verbose: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        runtimeCaching: [
-          {
-            handler: 'networkFirst',
-            urlPattern: /^https?.*/,
-          },
-        ],
-      })
-    )
-
+  webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new NextWorkboxPlugin({
+          buildId,
+          distDir,
+          importWorkboxFrom: 'cdn',
+          swDest: 'service-worker.js',
+          swDestRoot: `.next/static/${buildId}/pages`,
+          swURLRoot: `_next/static/${buildId}/pages`,
+        })
+      )
+    }
     return config
   },
+  generateBuildId: async () => buildId,
 }
