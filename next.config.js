68fcf71ac850991843a03e1a4985a769909e449c
Neil Kistner <neil.kistner@gmail.com>
2020-02-24 11:25:37 -0600

    Switch to next-offline package

--- next.config.js
+++ next.config.js
@@ -1,22 +1,31 @@
-const NextWorkboxPlugin = require('next-workbox-webpack-plugin')
+const withOffline = require('next-offline')
 
-const buildId = '1'
-
-module.exports = {
-  webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
-    if (!isServer && !dev) {
-      config.plugins.push(
-        new NextWorkboxPlugin({
-          buildId,
-          distDir,
-          importWorkboxFrom: 'cdn',
-          swDest: 'service-worker.js',
-          swDestRoot: `.next/static/${buildId}/pages`,
-          swURLRoot: `_next/static/${buildId}/pages`,
-        })
-      )
-    }
-    return config
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  // generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
   },
-  generateBuildId: async () => buildId,
 }
+
+module.exports = withOffline(nextConfig)
