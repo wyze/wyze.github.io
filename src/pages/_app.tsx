ffb3493b3a638f35b4480f0ab98e343cc098db56
Neil Kistner <neil.kistner@gmail.com>
2020-02-24 07:30:28 -0600

    Fix some lighthouse things

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Neil Kistner | St. Louis Software Engineer</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="Neil Kistner is a self taught software engineer focused mainly on web applications. He loves open source and contributes to projects like Yarn and Tessel."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
