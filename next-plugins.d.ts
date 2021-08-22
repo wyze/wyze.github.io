aa45f3471d7a32cbcd3e2e5480aa25db3b59334a
Neil Kistner <neil.kistner@gmail.com>
2021-08-21 20:56:42 -0500

    Fix dependabot dependencies (#45)

--- next-plugins.d.ts
+++ next-plugins.d.ts
@@ -3,24 +3,3 @@ declare module 'next-offline' {
 
   export = offline
 }
-
-declare module 'next-optimized-images' {
-  const optimize: (
-    options?: Record<string, unknown>,
-    composePlugins?: Record<string, unknown>
-  ) => Record<string, unknown>
-
-  export = optimize
-}
-
-declare module 'next-compose-plugins' {
-  const compose: (
-    plugins: unknown[],
-    nextConfig?: Record<string, unknown>
-  ) => (
-    phase: string,
-    options: Record<string, unknown>
-  ) => Record<string, unknown>
-
-  export = compose
-}
