58bd8676dc8d6c1f3718f2e61fa187f3a455b5ca
Neil Kistner <neil.kistner@gmail.com>
2022-10-06 23:22:43 -0500

    Upgrade dependencies (#54)

--- next.config.ts
+++ next.config.ts
@@ -12,26 +12,4 @@ module.exports = withOffline({
     { source: '/projects', destination: '/' },
     { source: '/conclusion', destination: '/' },
   ],
-  target: 'serverless',
-  transformManifest: (manifest: string[]) => ['/'].concat(manifest),
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
 })
