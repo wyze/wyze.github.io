651248d9b21f6f9979cc70f2c3961c2bae654f72
Neil Kistner <neil.kistner@gmail.com>
2022-10-12 09:02:20 -0500

    Update ESLint configuration to match Remix configuration

--- app/components/Link.test.tsx
+++ app/components/Link.test.tsx
@@ -1,13 +1,13 @@
 import { Link } from './Link'
 import { css } from 'otion'
 import { describe, expect, it } from 'vitest'
-import { fireEvent, render, screen } from '~/test/test-utils'
+import { render, screen, setup } from '~/test/test-utils'
 
 describe('<Link />', () => {
   it('should render', () => {
-    render(<Link href="https://mock.local/1">Content</Link>)
+    const { user } = setup(<Link href="https://mock.local/1">Content</Link>)
 
-    fireEvent.click(screen.getByText('Content'))
+    user.click(screen.getByText('Content'))
 
     expect(screen.getByText('Content')).toBeInTheDocument()
     expect(screen.getByText('Content')).toHaveAttribute(
--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,2 +1,11 @@
 // re-export everything
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 export * from '@testing-library/react'
+
+export function setup(jsx: React.ReactElement) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  }
+}
